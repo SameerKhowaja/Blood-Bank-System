ea0e9e0c7b91fac5669b3aba5b9dc5ad3fe73756
Sameer Khowaja - SK <54762751+SameerKhowaja@users.noreply.github.com>
2021-03-28 20:39:27 +0500

    bugs fixed

---
+++ BloodBank1/Controllers/DashboardController.cs
@@ -0,0 +1,52 @@
+﻿using BloodBank1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodBank1.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NewDonor()
+        {
+            return View();
+        }
+
+        public IActionResult BloodDonors()
+        {
+            return View();
+        }
+
+        public IActionResult BloodRequest()
+        {
+            return View();
+        }
+
+        public IActionResult BloodRecievers()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
